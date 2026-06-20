77a817624f3624a8803239dbec85525e49877b8d
Husam Burhan <husam.burhan@gmail.com>
2026-06-20 14:02:37 +0300

    fix: fixing adKid function

--- kids/kids.js
+++ kids/kids.js
@@ -2,18 +2,24 @@ import createSupabaseClient from "../utils/create-supabase-client.js";
 
 export async function addKid(req, res, next) {
     const full_name = req.body.full_name;
-    const user_id = req.body.user_id;
+    const user_id = req.body.user_id ?? req.user.id;
     const classroom = req.body.classroom;
-    const is_confirmed = false;
+    const is_confirmed = req.user.role === 'admin';
 
     const client = await createSupabaseClient();
 
-    await client.from('kids').insert({
+    const {error} = await client.from('kids').insert({
         full_name,
         user_id,
         classroom,
         is_confirmed
     });
+
+    if(error){
+        return res.status(500).send(error);
+    }
+
+    return res.sendStatus(200);
 }
 
 export async function getKidsOf(req, res, next) {
