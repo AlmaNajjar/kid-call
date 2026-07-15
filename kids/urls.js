8f2ffd987c1d0ae853ce9ff3876ae17e9f0f4753
Alma Najjar <almanajjar01@gmail.com>
2026-07-15 09:10:37 +0300

    Resolve conflicts in urls.js and clean up Git merge markers for task_5

--- kids/urls.js
+++ kids/urls.js
@@ -1,40 +1,11 @@
-import express from 'express';
-<<<<<<< HEAD
-<<<<<<< HEAD
+    import express from 'express';
 import { addKid, getKidsOf, getAllKids, callKid, confirmKid } from './kids.js';
 import { validateAddingKid, validateGetKidsOf, validateCallKid, validateConfirmKid } from './validators.js';
-=======
-import { addKid, getKidsOf , callKid } from './kids.js';
-import { validateAddingKid, validateGetKidsOf , validateCallKid } from './validators.js';
->>>>>>> origin/main
-=======
-import { addKid, getKidsOf, getAllKids, callKid } from './kids.js';
-import { validateAddingKid, validateGetKidsOf, validateCallKid } from './validators.js';
->>>>>>> origin/main
 
 export const router = express.Router();
 
 router.post('/', validateAddingKid, addKid);
-<<<<<<< HEAD
-<<<<<<< HEAD
 router.get('/:id', validateGetKidsOf, getKidsOf);
-
 router.post('/:id/call', validateCallKid, callKid);
-
 router.get('/admin/all', getAllKids);
-
 router.patch('/:id/confirm', validateConfirmKid, confirmKid);
-=======
-router.post("/:id/call", validateCallKid, callKid);
-
-=======
->>>>>>> origin/main
-router.get('/:id', validateGetKidsOf, getKidsOf);
-
-router.post('/:id/call', validateCallKid, callKid);
-
-<<<<<<< HEAD
->>>>>>> origin/main
-=======
-router.get('/admin/all', getAllKids);
->>>>>>> origin/main
